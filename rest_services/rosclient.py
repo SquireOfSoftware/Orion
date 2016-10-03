89051af39ad3867a3ebd06a24cb1c8aa40773599
KCasareo <Kevin.Casareo@student.uts.edu.au>
2016-10-03 14:31:11 +1100

    Moved all to rest_services

---
+++ rest_services/rosclient.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python
+
+from abc import ABCMeta
+
+class ROSClient(object):
+    __metaclass__ = ABCMeta
+    def __init__(self):
+
+    @abstractmethod
+    def subscribe(self): pass
+    #Subscribe target for this class
+
+    def callback(self): pass
+    #Callback method 
+
+    @classmethod
+    def __subclasshook__(cls, C)
+        if cls is ROSClient:
+            if any ("callback" in B.dict for B in C.__mro__):
+                return true
+        return 
+
+
+
+ROSClient.register()
