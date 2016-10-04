9bd8654ba3cd25abbc55467ca1d5ff927c39e963
KCasareo <Kevin.Casareo@student.uts.edu.au>
2016-10-04 16:09:50 +1100

    Squash this ++fixd;

--- orion_project/urls.py
+++ orion_project/urls.py
@@ -21,6 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^drone/', include('drone.urls')),
-]
-=======
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
