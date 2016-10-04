15516054be10ef6f2b52d724b1535342d165f732
KCasareo <Kevin.Casareo@student.uts.edu.au>
2016-10-04 14:14:27 +1100

    Added apache2 file directory setup

--- orion_project/wsgi.py
+++ orion_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Orion project.
+WSGI config for orion_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Orion.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orion_project.settings")
 
 application = get_wsgi_application()
