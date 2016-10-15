4eadd36ff35cb6bc8dfd3fc80c964474882054f3
KCasareo <Kevin.Casareo@student.uts.edu.au>
2016-10-15 17:58:38 +1100

    Drone Control test method added Do not touch flighttester because its garbage

---
+++ experiments/flight_tester.py
@@ -0,0 +1,3 @@
+#!usr/bin/env python
+#DOES NOT FUCKING WORK
+from rest.services.drone_control import *
--- rest/services/drone_control.py
+++ rest/services/drone_control.py
@@ -5,29 +5,31 @@ import rospy;
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
+import time;
 class drone_control (object):
+
     def __init__(self):
-        self.move = rospy.Publisher('/cmd_vel', Twist, queue_size = 20)
-        self.land = rospy.Publisher('/ardrone/land', Empty, queue_size = 5)
-        self.emergency = rospy.Publisher('/ardrone/emergency', Empty, queue_size = 5)
-        self.takeoff = rospy.Publisher('/ardrone/takeoff', Empty, queue_size = 5)
+        self._move = rospy.Publisher('/cmd_vel', Twist, queue_size = 20)
+        self._land = rospy.Publisher('/ardrone/land', Empty, queue_size = 5)
+        self._emergency = rospy.Publisher('/ardrone/emergency', Empty, queue_size = 5)
+        self._takeoff = rospy.Publisher('/ardrone/takeoff', Empty, queue_size = 5)
         rospy.init_node('orion_controller', anonymous=True)
         rate = rospy.Rate(100)
 
 
-    def drone_land(self):
-        self.land.publish(Empty())
+    def land(self):
+        self._land.publish(Empty())
         return
 
-    def drone_emergency(self):
-        self.emergency.publish(Empty())
+    def emergency(self):
+        self._emergency.publish(Empty())
         return
 
-    def drone_takeoff(self):
-        self.takeoff.publish(Empty())
+    def takeoff(self):
+        self._takeoff.publish(Empty())
         return
 
-    def drone_move(self, data):
+    def move(self, data):
         self.linear = data.linear
         self.angular = data.angular
         self.move.publish(Twist(Vector3(data.linear.x,
@@ -36,3 +38,8 @@ class drone_control (object):
                                 Vector3(data.angular.x,
                                         data.angular.y,
                                         data.angular.z)))
+    def test(self):
+        self.takeoff()
+        #Takes more time
+        time.sleep(10)
+        self.land()
