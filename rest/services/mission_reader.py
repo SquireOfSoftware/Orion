9fe0658a77cfb45d2b7c67017e587e34fd0fb808
Joseph Tran <fred.smoth@gmail.com>
2016-10-22 13:11:13 +1100

    Starting to work on the mission reader

--- rest/services/mission_reader.py
+++ rest/services/mission_reader.py
@@ -2,6 +2,10 @@
 
 from resource_locator import resource, resource_locator
 from database_access_layer import connector
+
 class mission_reader(connector, resource):
     def __init__(self, resource_locator):
         super(resource_locator);
+
+    def read(self):
+        connector.connect()
