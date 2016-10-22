01260e67f8181408b15dd16a723fd94160d72537
Joseph Tran <fred.smoth@gmail.com>
2016-10-22 15:10:53 +1100

    Able to talk to the database

--- rest/services/main.py
+++ rest/services/main.py
@@ -1,11 +1,24 @@
-from resource_locator import resource_locator
-from drone_metadata import drone_metadata
-from drone_control import drone_control
-from drone_media import drone_media
-
-def __main__():
-
-
+#from resource_locator import resource_locator
+#from drone_metadata import drone_metadata
+#from drone_control import drone_control
+#from drone_media import drone_media
+from database_access_layer import connector
+#from rest.management_constants import MISSION_STATUS
 
 
-    return
+testConnector = connector()
+
+testConnector.connect()
+
+testQuery = "select MissionID, Drone_DroneID " \
+            "from Mission " \
+            "where MissionStatus_MissionStatustID = (select MissionStatusID " \
+                                                    "from MissionStatus " \
+                                                    "where MissionStatusName = 'QUEUED');"
+testCursor = testConnector.cursor
+testCursor.execute(testQuery)
+
+for (MissionID, Drone_DroneID) in testCursor:
+    print("{} {}".format(MissionID, Drone_DroneID))
+
+testConnector.disconnect()
