659e74dbd026b0b3690ee7bb73f32db4462c85bd
Joseph Tran <fred.smoth@gmail.com>
2016-10-29 13:20:26 +1100

    Able to poll a set of things using $interval

--- web-gui/js/specific-pages/missionExecutor.js
+++ web-gui/js/specific-pages/missionExecutor.js
@@ -1,19 +1,38 @@
 /**
- * Created by Joseph Tran on 27/10/2016.
+ * Created by Joseph Tran on 29/10/2016.
+ * This is a helpful REST API
+ * https://thinkster.io/django-angularjs-tutorial
+ *
+ * AngularJS tutorial
+ * https://www.toptal.com/angular-js/a-step-by-step-guide-to-your-first-angularjs-app
  */
 
 var webServer = angular.module("webServer", [])
-.controller("missionStarterCtrl", function($log, $http, $scope) {
-    $scope.missions = [];
+.controller("missionExecutionCtrl", function($log, $http, $interval, $scope)
+{
+    /* DEFAULT scope bindings */
+    $scope.baseurl = "http://localhost:5001/rest/";
+    var RESTMISSIONS = "missions";
+    var RESTDRONES = "drones";
+
     $scope.errorMsg = {
         heading: "ERROR",
         message: []
     };
 
-    $scope.showCanvas = true;
+    $scope.warningMsg = {
+        heading: "Warning",
+        message: []
+    };
 
-    var baseurl = "http://localhost:5001/rest/";
-    var RESTMISSION = "missions";
+    var config = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+        'Access-Control-Max-Age': '3600',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Content-Type': 'application/text',
+        'Access-Control-Allow-Credentials': false
+    };
 
     var MINGRID = 0;
     var MAXGRID = 300;
@@ -34,10 +53,6 @@ var webServer = angular.module("webServer", [])
         $scope.errorMsg.message = [];
     };
 
-    $scope.loadMission = function () {
-        window.location.href = "mission-executor.html";
-    };
-
     function showSuccessScreen (){
         jQuery(".loading").hide();
         jQuery(".success").show();
@@ -51,56 +66,19 @@ var webServer = angular.module("webServer", [])
         $scope.errorMsg.message.push(message);
     }
 
-    var config = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
-        'Access-Control-Max-Age': '3600',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-        'Content-Type': 'application/text',
-        'Access-Control-Allow-Credentials': false
+    $scope.closeWarningScreen = function (){
+        jQuery(".warning").hide();
+        $scope.warningMsg.message = [];
     };
 
-    function showMissionListing() {
-        jQuery("#mission-listing").show();
-    }
-
-    $scope.init = function () {
-        getAllMission();
-    };
-
-    function getAllMission() {
-        toggleLoadingScreen();
-        $http.get(baseurl + RESTMISSION)
-            .then(function(data) {
-                $log.debug(data.data);
-                toggleLoadingScreen();
-                //showSuccessScreen();
-                var i = 0;
-                for (i = 0; i < data.data.length; i++) {
-                    loadUpMission(data.data[i]);
-                }
-                showMissionListing();
-            })
-            .catch(function(data) {
-                displayError(data);
-            });
-    }
-
-    function displayError(data) {
-        addErrorMessage("Failed to send GET request");
-        addErrorMessage(data);
-        $log.debug(data);
-        toggleLoadingScreen();
-        showErrorScreen();
-    }
-
-    function loadUpMission(mission) {
-        $scope.missions.push(mission);
-        $log.debug(mission.mission.id);
+    function addWarningMessage(message) {
+        $scope.warningMsg.message.push(message);
     }
 
     function drawUpGrid(mission) {
-        var gridContext = jQuery("#mission-" + mission.mission.id)[0].getContext("2d");
+        // TODO get current mission
+        // TODO get canvas grid
+        //var gridContext = jQuery("#mission-" + mission.mission.id)[0].getContext("2d");
         $log.debug(gridContext);
         drawHorizontalLines(gridContext);
         drawVerticalLines(gridContext);
@@ -108,30 +86,6 @@ var webServer = angular.module("webServer", [])
         drawDot(mission.point_of_interest, gridContext, ORANGE);
     }
 
-    $scope.startMission = function(missionId) {
-        $log.debug("Trying to start missiong with id: " + missionId);
-        $http.put(baseurl + RESTMISSION + "/" + missionId + "/start", {data: ""}, config)
-            .then(function() {
-                showSuccessScreen();
-            })
-            .catch(function(data) {
-                displayError(data);
-            });
-    };
-
-    $scope.$on("onLastRepeatItem", function (scope, element, attrs) {
-        var i = 0;
-        for (i = 0; i < $scope.missions.length; i++) {
-            var mission = $scope.missions[i];
-            drawUpGrid(mission);
-        }
-    });
-
-    $scope.createAMission = function () {
-        window.href.location = "mission-creator.html"
-    };
-
-    /*Code from Mission Controller*/
     function drawDot(mouseClick, canvasGrid, colour) {
         canvasGrid.beginPath();
         canvasGrid.arc(mouseClick.x, mouseClick.y, 2, 0, 2 * Math.PI, true);
@@ -182,14 +136,38 @@ var webServer = angular.module("webServer", [])
         return (object !== undefined) &&
             (object !== null);
     }
-});
 
-webServer.directive("onLastRepeatItem", function () {
-    return function(scope, element, attrs){
-        if (scope.$last) {
-            setTimeout(function () {
-                scope.$emit("onLastRepeatItem", element, attrs)
-            }, 1);
-        }
+    /* FINISHED DEFAULT BINDINGS */
+
+    $scope.init = function() {
+        getCurrentMission();
+    };
+
+    function getCurrentMission() {
+        toggleLoadingScreen();
+        $http.get($scope.baseurl + RESTMISSIONS)
+            .then(function (data) {
+                $log.debug(data);
+                toggleLoadingScreen();
+                $interval(pollCurrentMission, 5000);
+            })
+            .catch(function (data) {
+                $log.error(data);
+                addErrorMessage(data);
+                showErrorScreen();
+            });
     }
+
+    function pollCurrentMission() {
+
+        $http.get($scope.baseurl + RESTMISSIONS)
+            .then(function (data) {
+                $log.debug(data);
+            })
+            .catch(function(data) {
+                $log.error("failed to poll");
+            });
+    }
+
+    // Look up interval angular
 });
