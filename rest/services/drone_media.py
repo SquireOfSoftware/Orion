80c8840c6234ac2d64f6f7e6ef80a5f1166ffeac
KCasareo <Kevin.Casareo@student.uts.edu.au>
2016-10-31 16:33:33 +1100

    Fixed stuff

--- rest/services/drone_media.py
+++ rest/services/drone_media.py
@@ -2,6 +2,7 @@
 import rospy
 import cv2
 import logging
+import base64
 from sensor_msgs.msg import Image
 from database_access_layer import *
 from resource_locator import resource_locator, resource
@@ -18,14 +19,14 @@ class drone_media(connector, resource):
         self.bridge = CvBridge()
         return
 
-
-
     def image_callback(data):
         try:
             cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
         except CvBridgeError, e:
             print(e)
         else:
-            img_str = cv2.imencode('.png', cv2_img)[1].tostring()
-
+            img = cv2.imencode('.png', cv2_img)[1]
+            img_str = base64.encodestring(img)
         #Write to DB
+        sql = "INSERT INTO Image (ImageTimestamp, ImageBlob, Mission_MissionID) VALUES (unix_timestamp(), %(image)s, (SELECT MissionID from Mission WHERE MissionStatus_MissionStatusID = 2 LIMIT 1) );"
+        self.cursor.execute(sql, img_str)
