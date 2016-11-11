fdc287a79d5be7718b42963ae21fef46726d501a
Joseph Tran <fred.smoth@gmail.com>
2016-11-11 15:11:43 +1100

    Adding in sample data

---
+++ db model/sql sample data/Missions.sql
@@ -0,0 +1,5 @@
+MissionID,MissionStartTime,MissionEndTime,MissionCreationDate,MissionAltitude,MissionStatus_MissionStatusID,Drone_DroneID
+3,NULL,NULL,"2016-10-27 10:15:35",50,1,1
+4,NULL,NULL,"2016-11-07 03:03:53",50,1,1
+5,NULL,NULL,"2016-11-08 00:42:15",50,1,1
+6,NULL,NULL,"2016-11-08 01:28:16",50,2,1
---
+++ db model/sql sample data/POI.sql
@@ -0,0 +1,5 @@
+PointOfInterestIDPointOfInterestID,PointOfInterestX,PointOfInterestY,Mission_MissionID
+2,137.727,128.523,3
+3,-5.64706,24.3529,4
+4,6.71053,-35.5263,5
+5,12.5156,18.8667,6
